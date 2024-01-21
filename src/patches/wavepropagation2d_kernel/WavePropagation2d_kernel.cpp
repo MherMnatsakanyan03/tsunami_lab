161e32dbf32782fcc9905846f07564687528ec8b
= <=>
2024-01-21 14:41:51 +0100

    formatting

--- src/patches/wavepropagation2d_kernel/WavePropagation2d_kernel.cpp
+++ src/patches/wavepropagation2d_kernel/WavePropagation2d_kernel.cpp
@@ -18,8 +18,6 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
-// #include </Users/ibyton/Desktop/Uni/Opencl/OpenCL/common/inc/CL/cl.h>
-#include <CL/cl.h>
 #include "../../solvers/f-wave/F_wave.h"
 
 cl_device_id create_device()
