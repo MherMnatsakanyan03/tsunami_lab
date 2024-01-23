aae4f2ea502538946ffab8e34b8aa5892c2b59e2
Mher Mnatsakanyan <mher.mnatsakanyan03@gmail.com>
2024-01-23 18:02:29 +0100

    added both paths

--- src/patches/wavepropagation2d_kernel/WavePropagation2d_kernel.cpp
+++ src/patches/wavepropagation2d_kernel/WavePropagation2d_kernel.cpp
@@ -9,7 +9,8 @@
 #define CL_TARGET_OPENCL_VERSION 300
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
 #define PROGRAM_FILE "/Users/ibyton/Desktop/Uni/tsunami_lab/build/src/patches/wavepropagation2d_kernel/kernel.cl"
-// #define PROGRAM_FILE "kernel.cl"
+// #define PROGRAM_FILE "/home/mnatsakanyan/Uni/TsunamiLab/tsunami_lab/src/patches/wavepropagation2d_kernel/kernel.cl"
+//  #define PROGRAM_FILE "kernel.cl"
 #define KERNEL_X_AXIS_FUNC "updateXAxisKernel"
 #define KERNEL_Y_AXIS_FUNC "updateYAxisKernel"
 #define KERNEL_UPDATE_CELLS "updateCellsKernel"
